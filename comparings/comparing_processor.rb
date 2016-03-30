c5ec17944e8bd7e5b312df17a0e83e0a29e7c9b8
Kurakin Alexander <kuraga333@mail.ru>
2016-03-30 22:25:51 +0300

    Add filtering (turned off) of useless regions or generations

--- comparings/comparing_processor.rb
+++ comparings/comparing_processor.rb
@@ -7,6 +7,9 @@ module Comparings
 
   class ComparingProcessor < Processor
 
+    USEFUL_GENERATIONS = [1923..1942, 1943..1962, 1963..1982, 1983..2002]
+    USEFUL_REGIONS = ['Московская область', 'Самарская область', 'Забайкальский край']
+
     def process
       data_dup.each_value do |unit|
         # A. Год рождения / Возраст
@@ -17,6 +20,25 @@ module Comparings
 
         unit.replace UselessAnswersProcessor.new(unit).process
       end
+=begin
+     .select do |unit_name, unit|
+
+        USEFUL_GENERATIONS.collect do |generation|
+          begin
+            birthyear = Integer unit['questionA']
+          rescue ArgumentError, TypeError
+            false
+          else
+            generation.include? birthyear
+          end
+        end.any?
+
+      end.select do |unit_name, unit|
+
+        USEFUL_REGIONS.include? unit['questionC']
+
+      end
+=end
     end
 
   end
