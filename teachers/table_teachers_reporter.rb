678281fc460bda7a99bc765259bfed0123eae4ee
Kurakin Alexander <kuraga333@mail.ru>
2016-04-13 16:31:50 +0300

    Optimize table reporters

--- teachers/table_teachers_reporter.rb
+++ teachers/table_teachers_reporter.rb
@@ -15,21 +15,7 @@ module Teachers
     end
 
     def report
-      question_names = @data.values.collect(&:keys).flatten.uniq
-      unit_names = @data.keys
-
-      CSV $stdout do |csv|
-        row = [''] + unit_names.collect(&method(:process_unit_name))
-        csv << row
-
-        question_names.each do |question_name|
-          row = [@descriptions[question_name]]
-          unit_names.each do |unit_name|
-            row << process_answer(@data[unit_name][question_name])
-          end
-          csv << row
-        end
-      end
+      report_csv
     end
 
   end
