cb79e812d34fa7f63377ed5ed5833d29cf70955a
Kurakin Alexander <kuraga333@mail.ru>
2016-05-05 15:08:56 +0300

    birthday_processor: Process 2000s, too

--- lib/processors/birthyear_processor.rb
+++ lib/processors/birthyear_processor.rb
@@ -7,6 +7,7 @@ class BirthyearProcessor < Processor
   def process
     case @data
       when /((?:19|20)\d{2})/ then $1
+      when /0(\d)/            then "200#{$1}"
       when /(\d{1,2})/        then String(Date.today.year-Integer($1))
       else ''
     end
